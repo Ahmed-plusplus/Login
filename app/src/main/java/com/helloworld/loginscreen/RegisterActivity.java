b948512116b176ac4bf5b7c72539dd4e2f7775aa
Ahmed_plus_plus <ahmed8576498@gmail.com>
2020-09-12 02:47:06 +0200

    change from Handler to AsyncTask

--- app/src/main/java/com/helloworld/loginscreen/RegisterActivity.java
+++ app/src/main/java/com/helloworld/loginscreen/RegisterActivity.java
@@ -75,127 +75,127 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                AlertDialog.Builder alertBuilder;
-                final AlertDialog dialog;
-
-                alertBuilder = new AlertDialog.Builder(RegisterActivity.this);
-                alertBuilder.setCancelable(false);
-                LayoutInflater inflater = LayoutInflater.from(RegisterActivity.this);
-                alertBuilder.setView(inflater.inflate(R.layout.wait_progress,null));
-
-                dialog = alertBuilder.create();
-
-                final Handler handleToast = new Handler(){
-                    @Override
-                    public void handleMessage(@NonNull Message msg) {
-                        Toast.makeText(getApplicationContext(), (String)msg.obj, Toast.LENGTH_SHORT).show();
-                    }
-                };
-
-                final Handler startProgressAlert = new Handler() {
-                    @Override
-                    public void handleMessage(@NonNull Message msg) {
-                        dialog.show();
-                    }
-                };
-
-                final Handler endProgressAlert = new Handler() {
-                    @Override
-                    public void handleMessage(@NonNull Message msg) {
-                        dialog.dismiss();
-                    }
-                };
-
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        startProgressAlert.sendEmptyMessage(0);
-
-                        String username,email,password;
-                        username = name.getText().toString();
-                        email = mail.getText().toString();
-                        password = pass.getText().toString();
-                        try {
-                            if(username.isEmpty() || email.isEmpty() || password.isEmpty())
-                                throw new Exception("Fill the empty fields");
-                            /*
-                            int index = email.indexOf("@");
-
-                            if(index == -1)
-                                throw new Exception("Invalid mail");
-
-                            String nameAddress,pathAddress;
-                            nameAddress = email.substring(0,index);
-                            pathAddress = email.substring(index+1);
-
-                            if(nameAddress.isEmpty() || pathAddress.isEmpty())
-                                throw new Exception("Invalid mail");
-
-                            for(int i=0; i<nameAddress.length(); i++)
-                                if(!(nameAddress.charAt(i)>='A' && nameAddress.charAt(i)<='Z'
-                                        || nameAddress.charAt(i)>='a' && nameAddress.charAt(i)<='z'
-                                        || nameAddress.charAt(i)>='0' && nameAddress.charAt(i)<='9'
-                                        || nameAddress.charAt(i)=='_'))
-                                    throw new Exception("Invalid mail");
-
-                            if(pathAddress.charAt(0)=='.'
-                                    || pathAddress.charAt(pathAddress.length()-1)=='.'
-                                    || !pathAddress.contains("."))
-                                throw new Exception("Invalid mail");
-
-                            for(int i=0; i<pathAddress.length()-1; i++)
-                                if (pathAddress.charAt(i)=='.' && pathAddress.charAt(i+1)=='.'
-                                    || !(pathAddress.charAt(i)>='a' && pathAddress.charAt(i)<='z' || pathAddress.charAt(i)=='.'))
-                                    throw new Exception("Invalid mail");
-                            //*/
-
-                            ///*
-                            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
-                                throw new Exception("Invalid mail");
-                            //*/
-                            if(password.length()<6 || password.length()>20)
-                                throw new Exception("The password must have length from 6 to 20");
-
-                            if(!(male.isChecked() || female.isChecked()) || !accept.isChecked())
-                                throw new Exception("The required data isn't completed");
-
-                            if(db.getUser(email)!=null)
-                                throw new Exception("This mail exists, you can't register by it another time");
-
-                            UserAuth user;
-
-                            if (bitmap != null) {
-                                String img_file = System.currentTimeMillis() + "";
-                                fos = openFileOutput(img_file, MODE_PRIVATE);
-                                fos.write(UserAuth.bitmapToByteArray(bitmap));
-                                fos.close();
-                                user = new UserAuth(username, email, password, ((male.isChecked()) ? 'M' : 'F'), img_file);
-                            } else
-                                user = new UserAuth(username, email, password, ((male.isChecked()) ? 'M' : 'F'), null);
-
-                            if (db.addUser(user) <= 0)
-                                throw new Exception("Error happened through registeration, please try again");
-
-                            Message msg = new Message();
-                            msg.obj = "Registeration is done successfully";
-                            handleToast.sendMessage(msg);
-
-                            Intent in = new Intent();
-                            in.putExtra(MainActivity.EMAIL,email);
-                            in.putExtra(MainActivity.PASS,password);
-                            setResult(RESULT_OK,in);
-
-                            finish();
-
-                        }catch (Exception e){
-                            Message msg = new Message();
-                            msg.obj = e.getMessage();
-                            handleToast.sendMessage(msg);
-                        }
-                        endProgressAlert.sendEmptyMessage(0);
-                    }
-                }).start();
-
+//                AlertDialog.Builder alertBuilder;
+//                final AlertDialog dialog;
+//
+//                alertBuilder = new AlertDialog.Builder(RegisterActivity.this);
+//                alertBuilder.setCancelable(false);
+//                LayoutInflater inflater = LayoutInflater.from(RegisterActivity.this);
+//                alertBuilder.setView(inflater.inflate(R.layout.wait_progress,null));
+//
+//                dialog = alertBuilder.create();
+//
+//                final Handler handleToast = new Handler(){
+//                    @Override
+//                    public void handleMessage(@NonNull Message msg) {
+//                        Toast.makeText(getApplicationContext(), (String)msg.obj, Toast.LENGTH_SHORT).show();
+//                    }
+//                };
+//
+//                final Handler startProgressAlert = new Handler() {
+//                    @Override
+//                    public void handleMessage(@NonNull Message msg) {
+//                        dialog.show();
+//                    }
+//                };
+//
+//                final Handler endProgressAlert = new Handler() {
+//                    @Override
+//                    public void handleMessage(@NonNull Message msg) {
+//                        dialog.dismiss();
+//                    }
+//                };
+//
+//                new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        startProgressAlert.sendEmptyMessage(0);
+//
+//                        String username,email,password;
+//                        username = name.getText().toString();
+//                        email = mail.getText().toString();
+//                        password = pass.getText().toString();
+//                        try {
+//                            if(username.isEmpty() || email.isEmpty() || password.isEmpty())
+//                                throw new Exception("Fill the empty fields");
+//                            /*
+//                            int index = email.indexOf("@");
+//
+//                            if(index == -1)
+//                                throw new Exception("Invalid mail");
+//
+//                            String nameAddress,pathAddress;
+//                            nameAddress = email.substring(0,index);
+//                            pathAddress = email.substring(index+1);
+//
+//                            if(nameAddress.isEmpty() || pathAddress.isEmpty())
+//                                throw new Exception("Invalid mail");
+//
+//                            for(int i=0; i<nameAddress.length(); i++)
+//                                if(!(nameAddress.charAt(i)>='A' && nameAddress.charAt(i)<='Z'
+//                                        || nameAddress.charAt(i)>='a' && nameAddress.charAt(i)<='z'
+//                                        || nameAddress.charAt(i)>='0' && nameAddress.charAt(i)<='9'
+//                                        || nameAddress.charAt(i)=='_'))
+//                                    throw new Exception("Invalid mail");
+//
+//                            if(pathAddress.charAt(0)=='.'
+//                                    || pathAddress.charAt(pathAddress.length()-1)=='.'
+//                                    || !pathAddress.contains("."))
+//                                throw new Exception("Invalid mail");
+//
+//                            for(int i=0; i<pathAddress.length()-1; i++)
+//                                if (pathAddress.charAt(i)=='.' && pathAddress.charAt(i+1)=='.'
+//                                    || !(pathAddress.charAt(i)>='a' && pathAddress.charAt(i)<='z' || pathAddress.charAt(i)=='.'))
+//                                    throw new Exception("Invalid mail");
+//                            //*/
+//
+//                            ///*
+//                            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+//                                throw new Exception("Invalid mail");
+//                            //*/
+//                            if(password.length()<6 || password.length()>20)
+//                                throw new Exception("The password must have length from 6 to 20");
+//
+//                            if(!(male.isChecked() || female.isChecked()) || !accept.isChecked())
+//                                throw new Exception("The required data isn't completed");
+//
+//                            if(db.getUser(email)!=null)
+//                                throw new Exception("This mail exists, you can't register by it another time");
+//
+//                            UserAuth user;
+//
+//                            if (bitmap != null) {
+//                                String img_file = System.currentTimeMillis() + "";
+//                                fos = openFileOutput(img_file, MODE_PRIVATE);
+//                                fos.write(UserAuth.bitmapToByteArray(bitmap));
+//                                fos.close();
+//                                user = new UserAuth(username, email, password, ((male.isChecked()) ? 'M' : 'F'), img_file);
+//                            } else
+//                                user = new UserAuth(username, email, password, ((male.isChecked()) ? 'M' : 'F'), null);
+//
+//                            if (db.addUser(user) <= 0)
+//                                throw new Exception("Error happened through registeration, please try again");
+//
+//                            Message msg = new Message();
+//                            msg.obj = "Registeration is done successfully";
+//                            handleToast.sendMessage(msg);
+//
+//                            Intent in = new Intent();
+//                            in.putExtra(MainActivity.EMAIL,email);
+//                            in.putExtra(MainActivity.PASS,password);
+//                            setResult(RESULT_OK,in);
+//
+//                            finish();
+//
+//                        }catch (Exception e){
+//                            Message msg = new Message();
+//                            msg.obj = e.getMessage();
+//                            handleToast.sendMessage(msg);
+//                        }
+//                        endProgressAlert.sendEmptyMessage(0);
+//                    }
+//                }).start();
+                new WaitProgress(RegisterActivity.this).execute();
             }
         });
 
@@ -286,6 +286,129 @@ public class RegisterActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         outState.clear();
         outState.putParcelable(IMAGE,bitmap);
+    }
+
+    class WaitProgress extends AsyncTask<Void,Void,Void>{
+
+        AlertDialog.Builder alertBuilder;
+        AlertDialog dialog;
+        Handler handleToast;
+
+        public WaitProgress(Context context){
+            alertBuilder = new AlertDialog.Builder(RegisterActivity.this);
+                    alertBuilder.setCancelable(false);
+            LayoutInflater inflater = LayoutInflater.from(RegisterActivity.this);
+                    alertBuilder.setView(inflater.inflate(R.layout.wait_progress,null));
+
+            dialog = alertBuilder.create();
+
+            handleToast = new Handler(){
+                @Override
+                public void handleMessage(@NonNull Message msg) {
+                    Toast.makeText(getApplicationContext(), (String)msg.obj, Toast.LENGTH_SHORT).show();
+                }
+            };
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            String username,email,password;
+            username = name.getText().toString();
+            email = mail.getText().toString();
+            password = pass.getText().toString();
+            try {
+                if(username.isEmpty() || email.isEmpty() || password.isEmpty())
+                    throw new Exception("Fill the empty fields");
+                            /*
+                            int index = email.indexOf("@");
+
+                            if(index == -1)
+                                throw new Exception("Invalid mail");
+
+                            String nameAddress,pathAddress;
+                            nameAddress = email.substring(0,index);
+                            pathAddress = email.substring(index+1);
+
+                            if(nameAddress.isEmpty() || pathAddress.isEmpty())
+                                throw new Exception("Invalid mail");
+
+                            for(int i=0; i<nameAddress.length(); i++)
+                                if(!(nameAddress.charAt(i)>='A' && nameAddress.charAt(i)<='Z'
+                                        || nameAddress.charAt(i)>='a' && nameAddress.charAt(i)<='z'
+                                        || nameAddress.charAt(i)>='0' && nameAddress.charAt(i)<='9'
+                                        || nameAddress.charAt(i)=='_'))
+                                    throw new Exception("Invalid mail");
+
+                            if(pathAddress.charAt(0)=='.'
+                                    || pathAddress.charAt(pathAddress.length()-1)=='.'
+                                    || !pathAddress.contains("."))
+                                throw new Exception("Invalid mail");
+
+                            for(int i=0; i<pathAddress.length()-1; i++)
+                                if (pathAddress.charAt(i)=='.' && pathAddress.charAt(i+1)=='.'
+                                    || !(pathAddress.charAt(i)>='a' && pathAddress.charAt(i)<='z' || pathAddress.charAt(i)=='.'))
+                                    throw new Exception("Invalid mail");
+                            //*/
+
+                ///*
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                    throw new Exception("Invalid mail");
+                //*/
+                if(password.length()<6 || password.length()>20)
+                    throw new Exception("The password must have length from 6 to 20");
+
+                if(!(male.isChecked() || female.isChecked()) || !accept.isChecked())
+                    throw new Exception("The required data isn't completed");
+
+                if(db.getUser(email)!=null)
+                    throw new Exception("This mail exists, you can't register by it another time");
+
+                UserAuth user;
+
+                if (bitmap != null) {
+                    String img_file = System.currentTimeMillis() + "";
+                    fos = openFileOutput(img_file, MODE_PRIVATE);
+                    fos.write(UserAuth.bitmapToByteArray(bitmap));
+                    fos.close();
+                    user = new UserAuth(username, email, password, ((male.isChecked()) ? 'M' : 'F'), img_file);
+                } else
+                    user = new UserAuth(username, email, password, ((male.isChecked()) ? 'M' : 'F'), null);
+
+                if (db.addUser(user) <= 0)
+                    throw new Exception("Error happened through registeration, please try again");
+
+                Message msg = new Message();
+                msg.obj = "Registeration is done successfully";
+                handleToast.sendMessage(msg);
+
+                Intent in = new Intent();
+                in.putExtra(MainActivity.EMAIL,email);
+                in.putExtra(MainActivity.PASS,password);
+                setResult(RESULT_OK,in);
+
+                finish();
+
+            }catch (Exception e){
+                Message msg = new Message();
+                msg.obj = e.getMessage();
+                handleToast.sendMessage(msg);
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void v) {
+            dialog.dismiss();
+            super.onPostExecute(v);
+        }
     }
 
 }
